1c9ad9212175d1625224f1a111a2ee53792f6f30
Mark Schramm <mschramm@stanford.edu>
2016-05-03 22:41:21 -0700

    map

--- grapher.js
+++ grapher.js
@@ -1,7 +1,31 @@
 var data; // a global
 
 d3.json("scpd_incidents.json", function(error, json) {
-  if (error) return console.warn(error);
-  data = json;
-  console.log(data);
+	if (error) return console.warn(error);
+	data = json;
+  
+
+	// Set up size
+	var width = 750,
+		height = width;
+
+	// Set up projection that map is using
+	var projection = d3.geo.mercator()
+		.center([-122.433701, 37.767683]) // San Francisco, roughly
+		.scale(225000)
+		.translate([width / 2, height / 2]);
+	// This is the mapping between <longitude, latitude> position to <x, y> pixel position on the map
+	// projection([lon, lat]) returns [x, y]
+	// projection.invert([x, y]) returns [lon, lat]
+
+	// Add an svg element to the DOM
+	var svg = d3.select("#chart").append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	// Add svg map at correct size, assumes map is saved in a subdirectory called "data"
+	svg.append("image")
+	          .attr("width", width)
+	          .attr("height", height)
+	          .attr("xlink:href", "sf-map.svg");
 });
